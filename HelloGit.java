b3c308d2498149d699f09c50ecee720c3a650d24
Ernesto <ernesto.vazquez.iglesias.alu@iesfernandoaguilar.es>
2023-02-15 08:36:12 +0100

    Primer commit subido

---
+++ HelloGit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hellogit;
+
+/**
+ *
+ * @author ernestovaig
+ */
+public class HelloGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world from GIT!");
+	System.out.println("Hey");        
+    }
+    
+}
